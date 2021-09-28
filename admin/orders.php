f42b0025a44b5ae126e10488dff455b3b740eecf
VimukthiniAttanayaka <VimU1999@#>
2021-09-28 14:13:00 +0530

    commint update2

--- admin/orders.php
+++ admin/orders.php
@@ -9,21 +9,50 @@ require_once('../connection.php');
     <meta charset="UTF-8">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>Cloths Section</title>
-    <style>
-        .container-fluid {
-            padding: 0px !important;
-            margin: 0px !important;
-        }
-    </style>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../styles/listing.css">
+    <title>Item List</title>
 </head>
 
 <body>
     <div class="container-fluid">
         <div class="row">
-        <a id='url' href="overview.php">Overview</a>
-        <a id='url' href="listing.php">Listings</a>
-        <a id='url' href="orders.php">Orders</a> 
+            <div class="col-1">
+                <a id='url' href="overview.php">Overview</a>
+            </div>
+            <div class="col-1">
+                <a id='url' href="listing.php">Listings</a>
+            </div>
+            <div class="col-1">
+                <a id='url' href="orders.php">Orders</a>
+            </div>
+        </div>
+        <div class="row">
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>Email</th>
+                    <th>List</th>
+                    <th>Address</th>
+                    <th>Total Price</th>
+                    <th>shipped</th>
+                    <th>delivered</th>
+                </tr>
+                <?php $query = "SELECT * FROM orders";
+                $select = mysqli_query($connection, $query);
+                while ($recodes = mysqli_fetch_assoc($select)) {
+                    ?>
+                        <tr>
+                            <td><?php echo $recodes['id']?></td>
+                            <td><?php echo $recodes['email']; ?></td>
+                            <td><table>
+                                <?php echo $recodes['list']; ?>
+                            </table></td>
+                            <td><?php echo $recodes['address']; ?></td>
+                            <td><?php echo $recodes['total_price']; ?></td>
+                        </tr>
+                <?php } ?>
+            </table>
         </div>
     </div>
 </body>
